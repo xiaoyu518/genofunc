5149a6a6ea2c462f9bd05a236d1a85906b01158a
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2022-02-26 15:58:18 +0000

    Update concatenate_fasta.py

--- genofunc/concatenate_fasta.py
+++ genofunc/concatenate_fasta.py
@@ -17,7 +17,6 @@ Copyright 2020 Xiaoyu Yu (xiaoyu.yu@ed.ac.uk).
 
 from Bio import SeqIO
 import glob
-import os
 from genofunc.utils import *
 
 def concatenate_fasta(in_prefix,gene_region,out_dir,log_file):
@@ -25,13 +24,14 @@ def concatenate_fasta(in_prefix,gene_region,out_dir,log_file):
 
     for genes in gene_region:
         fasta_files = glob.glob(in_prefix + "*" + genes + '*.fasta')
+        if fasta_files == []:
+            log_handle.write("Prefix does not contain " + genes + " files to concatenate.\n")
         output_file = out_dir + genes + ".fasta"
         out_fasta = open(output_file,"w")
         for files in fasta_files:
             for record in SeqIO.parse(files, "fasta"):
                 SeqIO.write(record, out_fasta, "fasta-2line")
-            log_handle.write(files + " has concatenated into " + output_file + "\n")
-            os.remove(files)
+            log_handle.write(files + " has concatenated into " + output_file + "\n")    
         close_handle(out_fasta)
 
     close_handle(log_handle)
