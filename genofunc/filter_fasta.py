d5bf5146ab2843d4c8649532ae05025e25f88280
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2022-02-26 17:31:34 +0000

    Update filter_fasta.py

--- genofunc/filter_fasta.py
+++ genofunc/filter_fasta.py
@@ -21,8 +21,8 @@ Copyright 2020 Xiaoyu Yu (xiaoyu.yu@ed.ac.uk).
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import glob
-import os
 import sys
+import os
 import math
 import csv
 from genofunc.utils import *
@@ -40,43 +40,47 @@ def filter_fasta(in_dir,in_metadata,gene_list,min_length,symmetric,out_dir,log_f
 
     for files in fasta_files:
         max_len = 0
-        output_file = out_dir + files[files.rfind("/")+1:]+"_filtered.fasta"
+        output_file = out_dir + files[files.rfind("/")+1:-6]+"_filtered.fasta"
         out_f = open(output_file,"w")
         for record in SeqIO.parse(files, "fasta"):
             if len(record.seq) > max_len:
                 max_len = len(record.seq)
-        min_length = math.trunc(min_length*max_len)
+        min_lengths = math.trunc(min_length*max_len)
+        flag = False
         for gene in gene_list:
             if files.find(gene) > -1:
-                if gene not in gene_dic.keys():
-                    gene_dic[gene] = []                        
+                gene_dic[gene] = []
+                file_gene = gene
+                flag = True
+        if flag == False:
+            log_handle.write("Folder does not contain " + gene + " file.\n")                       
         for record in SeqIO.parse(files, "fasta"):
-            if len(record.seq) >= min_length:
+            if len(record.seq) >= min_lengths:
                 if record.id.find("|") > -1:
                     record.id = record.id[:record.id.find("|")]
-                    gene_dic[gene].append(record.id)
-                    records = SeqRecord(record.seq, record.id, description= '')
-                    SeqIO.write(records, out_f, "fasta-2line")
+                gene_dic[file_gene].append(record.id)
+                records = SeqRecord(record.seq, record.id, description= '')
+                SeqIO.write(records, out_f, "fasta-2line")
             else:
                 log_handle.write("Sequence " + record.id + " contains less than " + str(min_length) + " base positions in file " + output_file + "\n")
         close_handle(out_f)
-        os.remove(files)
-        all_list.append(gene_dic[gene])
+        all_list.append(gene_dic[file_gene]) 
 
     filtered_files = glob.glob(out_dir + "*_filtered.fasta")
+
+    with open(in_metadata,"r") as f:
+        seperator = ','
+        if in_metadata.endswith('tsv'):
+            seperator = '\t'
+        reader = csv.reader(f,delimiter=seperator)
+        metadata = list(reader)
+    
+    os.remove(in_metadata)
 
     if symmetric is False:
         for files in filtered_files:
             os.rename(files,out_dir + files[files.rfind("/")+1:-15] + "_filter.fasta")
 
-        with open(in_metadata,"r") as f:
-            seperator = ','
-            if in_metadata.endswith('tsv'):
-                seperator = '\t'
-            reader = csv.reader(f,delimiter=seperator)
-            metadata = list(reader)
-
-        os.remove(in_metadata)
         metadata_dic = {}
 
         for gene in gene_dic.keys():
@@ -104,15 +108,7 @@ def filter_fasta(in_dir,in_metadata,gene_list,min_length,symmetric,out_dir,log_f
                     log_handle.write("Sequence " + record.id + " does not have a sequence for all three genome regions and is removed from sequence file.\n")
             close_handle(output_file)
             os.remove(files)
-
-        with open(in_metadata,"r") as f:
-            seperator = ','
-            if in_metadata.endswith('tsv'):
-                seperator = '\t'
-            reader = csv.reader(f,delimiter=seperator)
-            metadata = list(reader)
-
-        os.remove(in_metadata)
+        
         new_metadata = [metadata[0]]
 
         for rows in metadata:
