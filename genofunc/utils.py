6750ed670db5ef4805c5418c7338f902cd79980d
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2022-02-26 16:31:16 +0000

    Update utils.py

--- genofunc/utils.py
+++ genofunc/utils.py
@@ -18,22 +18,6 @@ def get_log_handle(log_file):
         log_handle = sys.stdout
     return log_handle
 
-def get_out_handle(out_fasta):
-    if out_fasta == '' or out_fasta == [] or out_fasta == ['']:
-        out_fasta = None
-    if not out_fasta:
-        out_handle = sys.stdout
-    else:
-        out_handle = open(out_fasta,"w")
-    return out_handle
-
-def get_in_handle(in_fasta):
-    if not in_fasta:
-        in_handle = sys.stdin
-    else:
-        in_handle = open(in_fasta)
-    return in_handle
-
 def close_handle(handle):
     if handle:
         handle.close()
