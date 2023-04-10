a279aad5aee06a6a3f0e6e72d2de507e82473a4d
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2023-04-10 14:15:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genofunc",
-    version="1.0.2",
+    version="1.0.4",
     packages=find_packages(),
     url="https://github.com/xiaoyu518/genofunc",
     license="MIT",
