ef5b01dd8b86ab0324604be0af1093b72910203e
Xiaoyu Yu <57363085+xiaoyu518@users.noreply.github.com>
2023-04-16 12:35:48 +0800

    Update reference_matcher.py

--- genofunc/reference_matcher.py
+++ genofunc/reference_matcher.py
@@ -3,7 +3,7 @@
 """
 Name: reference_matcher.py
 Author: Xiaoyu Yu
-Date: 01 April 2022
+Date: 16 April 2023
 Description: Map sequence to the closest reference sequence list based on mini-map2. 
 
 Options:
@@ -42,19 +42,55 @@ def reference_matcher(in_fasta,reference_sequence,out_fasta,log_file):
 
     for record in SeqIO.parse(in_fasta, "fasta"):
         matching_length = 0
-        mismatches = 8000
-        for hit in reference_list.map(record.seq):
-            if hit.mlen >= matching_length and hit.NM <= mismatches:
-                matching_length = hit.mlen
-                mismatches = hit.NM
-                reference_id = hit.ctg
-                start_ref = hit.r_st
-                end_ref = hit.r_en
-                cigar_array = hit.cigar
-            new_id = "|".join([record.id,str(matching_length),reference_id])
-            new_record = SeqRecord(record.seq,new_id,description="")
-        SeqIO.write(new_record, outfile, "fasta-2line")
-        log_handle.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(record.id,hit.ctg,matching_length,mismatches,start_ref,end_ref,cigar_array))
+        mismatches = round(float(percentage_match)*len(str(record.seq)),0)
+        if virus_type.upper() == "DNA":
+            for hit in reference_list.map(record.seq):
+                if hit.mlen >= matching_length and hit.NM <= mismatches:
+                    matching_length = hit.mlen
+                    mismatches = hit.NM
+                    reference_id = hit.ctg
+                    start_ref = hit.r_st
+                    end_ref = hit.r_en
+                    cigar_array = hit.cigar
+                if matching_length < mismatches:
+                    temp_seq = record.seq.reverse_complement()
+                    for hit in reference_list.map(temp_seq):
+                        if hit.mlen >= matching_length and hit.NM <= mismatches:
+                            reverse_matching_length = hit.mlen
+                            reverse_mismatches = hit.NM
+                            reverse_reference_id = hit.ctg
+                            reverse_start_ref = hit.r_st
+                            reverse_end_ref = hit.r_en
+                            reverse_cigar_array = hit.cigar
+                if reverse_matching_length > matching_length:
+                    new_id = "|".join([record.id,str(reverse_matching_length),reverse_reference_id])
+                    new_record = SeqRecord(temp_seq,new_id,description="")
+                    log_handle.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(record.id,hit.ctg,reverse_matching_length,reverse_mismatches,reverse_start_ref,
+                                                                           reverse_end_ref,reverse_cigar_array))
+                else:
+                    new_id = "|".join([record.id,str(matching_length),reference_id])
+                    new_record = SeqRecord(record.seq,new_id,description="")
+                    log_handle.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(record.id,hit.ctg,matching_length,mismatches,start_ref,end_ref,cigar_array))
+            if matching_length != 0 or reverse_matching_length != 0:
+                SeqIO.write(new_record, outfile, "fasta-2line")
+            
+        elif virus_type.upper() == "RNA":
+            for hit in reference_list.map(record.seq):
+                if hit.mlen >= matching_length and hit.NM <= mismatches:
+                    matching_length = hit.mlen
+                    mismatches = hit.NM
+                    reference_id = hit.ctg
+                    start_ref = hit.r_st
+                    end_ref = hit.r_en
+                    cigar_array = hit.cigar
+                new_id = "|".join([record.id,str(matching_length),reference_id])
+                new_record = SeqRecord(record.seq,new_id,description="")
+            if matching_length != 0:
+                SeqIO.write(new_record, outfile, "fasta-2line")
+            log_handle.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(record.id,hit.ctg,matching_length,mismatches,start_ref,end_ref,cigar_array))
+        else:
+            print("Virus type not valid, please re-enter correct virus type option (RNA/DNA). Program Exiting")
+            sys.exit()
     
     time_ran = dt.datetime.now() - time_start
     print("Time Lapse:", time_ran.total_seconds() / 60, "Minutes")
